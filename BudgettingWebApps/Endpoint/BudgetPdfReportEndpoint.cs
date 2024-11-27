3d3110cd4f4ba556d4ce25eb5bea27825bee6215
gabriel ndara <gabrielndara@gmail.com>
2024-11-27 15:30:31 +0200

    can download report based on real time

--- BudgettingWebApps/Endpoint/BudgetPdfReportEndpoint.cs
+++ BudgettingWebApps/Endpoint/BudgetPdfReportEndpoint.cs
@@ -1,11 +1,13 @@
+using BudgettingWebApps.Models.Request;
 using BudgettingWebApps.Reposiotories;
 using BudgettingWebApps.Services;
 using FastEndpoints;
+using Serilog;
 
 
 namespace BudgettingWebApps.Endpoint;
 
-public class BudgetPdfReportEndpoint : EndpointWithoutRequest
+public class BudgetPdfReportEndpoint : Endpoint<ReportRequest>
 {
     private readonly IincomeRepository _repository;
     private readonly IExpenseRepository _expenseRepository;
@@ -17,15 +19,21 @@ public class BudgetPdfReportEndpoint : EndpointWithoutRequest
 
     public override void Configure()
     {
-        Get("/budgeting/pdf/report");
+        Get("/budgeting/pdf/report/userId={currentuser:int}&month={monthNumber:int}");
     }
 
-    public override async Task HandleAsync(CancellationToken ct)
+    public override async Task HandleAsync(ReportRequest request,CancellationToken ct)
     {
         try
         {
+            var userId = request.CurrentUser;
+            var month = request.MonthNumber;
+           Log.Information($"user id {userId}: month {month}");
+           
+           Console.WriteLine($"console user id {userId}: months {month}");
+           
             var report = new QuestPdfService(_repository, _expenseRepository);
-            var byteArray = report.GenerateBudgetReportBytesAsync(2,7);
+            var byteArray = report.GenerateBudgetReportBytesAsync(userId,month);
           await SendBytesAsync(await byteArray, "IncomeReport.pdf", "application/pdf");
         }
         catch (Exception e)
---
+++ BudgettingWebApps/Models/Request/ReportRequest.cs
@@ -0,0 +1,7 @@
+namespace BudgettingWebApps.Models.Request;
+
+public class ReportRequest
+{
+    public int CurrentUser { get; set; }
+    public int MonthNumber { get; set; }
+}
